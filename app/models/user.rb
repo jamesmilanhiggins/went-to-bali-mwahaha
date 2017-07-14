67c4fe2262d5ec9c27226ce0823023ffec5aadd3
James Higgins <james.milan.higgins@gmail.com>
2017-07-14 14:05:19 -0700

    add validation for user password to be minimun 6 characters

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   has_secure_password
   has_many :orders
 
+  validates :password_digest, length: { minimum: 6 }, presence: true
+
+
+
   def previous_orders
     self.orders.where(status: 2).includes(order_items: :product)
   end

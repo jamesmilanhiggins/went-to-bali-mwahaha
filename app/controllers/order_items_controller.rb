65208a1ae276c28c9cdb0aebf66e471368d06b02
James Higgins <james.milan.higgins@gmail.com>
2017-07-14 13:00:02 -0700

    Add ajax to destroy item in cart

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -29,7 +29,11 @@ class OrderItemsController < ApplicationController
     @item = @order.order_items.find(params[:id])
     @item.destroy
     @order.save
+    # byebug
     redirect_to cart_path
+    respond_to do |format|
+      format.js
+    end
   end
 
   private

d656753b39f259938b93576735973ea3342fc6c4
James Higgins <james.milan.higgins@gmail.com>
2017-07-14 10:19:35 -0700

    add migration to add image column for products, and update seed file to add faker images, and show products on index page

---
+++ db/migrate/20170714164822_add_image_colum_to_products.rb
@@ -0,0 +1,5 @@
+class AddImageColumToProducts < ActiveRecord::Migration[5.0]
+  def change
+    add_column :products, :image, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,31 @@
-class Seed
+# class Seed
+  Product.destroy_all
+  User.destroy_all
+  # def self.begin
+    # seed = Seed.new
+    # seed.generate_products
 
-  def self.begin
-    seed = Seed.new
-    seed.generate_products
-  end
+    user = User.create(
+      name: "Admin",
+      email: "admin@gmail.com",
+      password_digest: "password",
+    )
 
-  def generate_products
+  # end
+
+  # def generate_products
     20.times do |i|
-      Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop)
+      Product.create!(name: Faker::Lorem.word,
+                      price: rand(10...100),
+                      description: Faker::Lorem.sentence(5, false, 0).chop,
+                      image: Faker::Avatar.image)
     end
-  end
-end
+    5.times do |i|
+      Order.create!(total_price: rand(10...100),
+                      user_id: rand(1...20)
+                    )
+    end
+#   end
+# end
 
-Seed.begin
+# Seed.begin
